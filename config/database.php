c6b057fd0ceda42005238b05576d2b91adc2d3a0
Assefa Demeke <assede1122@gmail.com>
2026-05-25 09:57:05 +0300

    first commit change

--- config/database.php
+++ config/database.php
@@ -31,18 +31,10 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db.aletcloud.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'test'),
             'username' => env('DB_USERNAME', 'admin_test'),
